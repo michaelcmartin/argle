2262d6efb8a4403b39a9880d6b707d73d50f082f
Michael Martin <mcmartin@gmail.com>
2012-11-10 17:59:36 -0800

    Finish up ARGLE 1.0, improving reporting and CLI.

--- Makefile
+++ Makefile
@@ -1,5 +1,28 @@
-argle: main.o module_map.o scan.o
-	g++ -o $@ main.o module_map.o scan.o
+CXX = g++
+CXXFLAGS = 
+
+LD = g++
+LDFLAGS = 
+
+CXXOBJS = main.o module_map.o scan.o
+OBJS = $(CXXOBJS)
+
+argle: $(CXXOBJS)
+	$(LD) $(LDFLAGS) -o argle $(OBJS)
 
 clean:
 	rm -f argle *.o
+
+depend:
+	find . -name \*.c\* | xargs makedepend -Y.
+
+$(CXXOBJS): %.o: %.cpp
+	$(CXX) $(CXXFLAGS) -c -o $@ $<
+
+
+# Makedepend results below
+# DO NOT DELETE
+
+./scan.o: scan.h
+./module_map.o: module_map.h
+./main.o: module_map.h scan.h
--- main.cpp
+++ main.cpp
@@ -8,6 +8,17 @@
 typedef std::set<std::string> UsedTokens;
 typedef std::map<std::string, UsedTokens> UseMap;
 
+namespace {
+
+bool verbose;
+
+void
+usage(const char *progname) {
+    std::cerr << "Usage:" << std::endl << std::endl;
+    std::cerr << "\t" << progname << " [-v] filenames" << std::endl << std::endl;
+    std::cerr << "Where -v is verbose output and \"filenames\" is the files to analyze." << std::endl;
+}
+
 class TokenPrinter : public ScannerCallback {
 public:
     TokenPrinter() { fill_map(modmap_); }
@@ -19,26 +30,67 @@ public:
         }
     }
     void dumpResults() {
-        bool first = true;
+        int minversion = 11;
+        std::set<std::string> exts;
+        
         for (UseMap::iterator it = result_.begin(); it != result_.end(); ++it) {
             if (it->second.size() > 0) {
-                if (first) {
-                    first = false;
+                // This is icky, and will collapse once we hit a major or
+                // minor version greater than 9
+                if (it->first.find("GL_VERSION_") == 0) {
+                    int len = it->first.size();
+                    int major = it->first[len-3] - '0';
+                    int minor = it->first[len-1] - '0';
+                    int version = major * 10 + minor;
+                    if (version > minversion) {
+                        minversion = version;
+                    }
                 } else {
-                    std::cout << std::endl;
-                }
-                std::cout << it->first << std::endl;
-                for (int j = 0; j < it->first.size(); ++j) {
-                    std::cout << "-";
-                }
-                std::cout << std::endl;
-                for (UsedTokens::iterator j = it->second.begin(); j != it->second.end(); ++j) {
-                    std::cout << *j << std::endl;
+                    exts.insert(it->first);
                 }
             }
         }
-        if (first) {
-            std::cout << "No functions found requiring a version of OpenGL later than 1.1." << std::endl;
+
+        std::cout << "Minimum required version of OpenGL: ";
+        std::cout << (minversion / 10) << "." << (minversion % 10) << std::endl;
+        if (exts.size() > 0) {
+            std::cout << "Required extensions: ";
+            bool first = true;
+            for (std::set<std::string>::iterator it = exts.begin();
+                 it != exts.end(); ++it) {
+                if (first) {
+                    first = false;
+                } else {
+                    std::cout << ", ";
+                }
+                std::cout << *it;
+            }
+            std::cout << std::endl;
+        }
+        if (verbose && result_.size() > 0) {
+            std::cout << std::endl << "Function usage" << std::endl;
+            std::cout << "--------------" << std::endl << std::endl;
+            
+            bool first = true;
+            for (UseMap::iterator it = result_.begin(); 
+                 it != result_.end(); ++it) {
+                if (it->second.size() > 0) {
+                    if (first) {
+                        first = false;
+                    } else {
+                        std::cout << std::endl;
+                    }
+                    std::cout << it->first << std::endl;
+                    for (int j = 0; j < it->first.size(); ++j) {
+                        std::cout << "-";
+                    }
+                    std::cout << std::endl;
+                    for (UsedTokens::iterator j = it->second.begin(); 
+                         j != it->second.end(); ++j) {
+                        std::cout << *j << std::endl;
+                    }
+                }
+            }
         }
     }
 private:
@@ -46,15 +98,33 @@ private:
     UseMap result_;
 };
 
+}
+
 int
 main(int argc, char **argv)
 {
     TokenPrinter tp;
+    bool did_work = false;
     for (int i = 1; i < argc; ++i) {
-        FILE *f = fopen(argv[i], "r");
-        scan(f, tp);
-        fclose(f);
+        std::string arg(argv[i]);
+        if (arg == "-v" || arg == "--verbose") {
+            verbose = true;
+        } else {
+            did_work = true;
+            FILE *f = fopen(argv[i], "r");
+            if (f) {
+                scan(f, tp);
+                fclose(f);
+            } else {
+                std::cerr << "Could not open " << argv[i] << std::endl;
+            }
+        }
     }
-    tp.dumpResults();
+    if (did_work) {
+        tp.dumpResults();
+    } else {
+        usage(argv[0]);
+        return 1;
+    }
     return 0;
 }
